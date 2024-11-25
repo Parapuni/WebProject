eb2503422bc2b05481e759860cd325cb6a1bbe06
Parapuni <Clearhapp@outlook.com>
2024-11-25 14:33:03 +0800

    完善响应filter

--- src/main/java/cmt/config/filter/ResponseCacheFilter.java
+++ src/main/java/cmt/config/filter/ResponseCacheFilter.java
@@ -5,9 +5,10 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
- * 控制浏览器缓存静态图片资源
+ * 控制浏览器缓存静态图片资源，设置寿命为2小时
  */
 @WebFilter(urlPatterns = "/*")
 public class ResponseCacheFilter implements Filter {
@@ -19,7 +20,8 @@ public class ResponseCacheFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        response.addHeader("Cache-Control","MAX-AGE=24*3600");
+        response.addHeader("Cache-Control","MAX-AGE="+2*3600);//最大寿命7200s = 2h
+        response.addHeader("Expires", new Date(System.currentTimeMillis()+1000*7200L).toString());//失效时间设置为当前时间+2h
         filterChain.doFilter(request,response);
     }
 
